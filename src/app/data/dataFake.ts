3c4e5933ef1001042d88bea641bed9c78c291ead
joseedivandro <jose.edivandro.ja@outlook.com>
2023-04-02 01:38:01 -0300

    top pages

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,38 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Aqueça seus aliados e queime seus inimigos com Milio, disponível na Atualização 13.6!",
+    "description": "Com apenas 12 anos, Milio tem um controle impressionante do axioma de fogo, dominando habilidades que já são consideradas raras em qualquer cidadão de Ixtal, ainda mais em alguém de sua idade. Enquanto a maioria dos ixtalenses passa a vida inteira estudando para entender os Axiomata — um sistema complexo, rígido e quase matemático de magia elementar —, Milio tem uma compreensão inerente de seus poderes e sabe que deve usá-los para o bem.",
+    "photoCover":"https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt70f9472266d26c08/64064e9a8bbba310615dd4e6/Faerie-Court-Milio---Final-optimized.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Volibear: a tempestade implacável",
+    "description": "Para aqueles que ainda o veneram, Volibear é a forma manifestada da tempestade. Destrutivo, selvagem e implacavelmente obstinado, ele já existia antes mesmo que os mortais andassem sobre a tundra de Freljord, protegendo as terras que ele e seus irmãos semideuses criaram. Com um ódio profundo pela civilização e pela fraqueza inerente a ela, Volibear luta para recuperar os velhos costumes de quando a terra era selvagem e o sangue jorrava livremente. Ele enfrenta todos aqueles que ousam entrar em seu caminho com dentes, garras e dominação estrondosa.",
+    "photoCover":"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Volibear_0.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Wukong: o rei macaco",
+    "description": "Wukong é um trapaceiro vastaya que usa sua força, agilidade e inteligência para confundir os oponentes e deixar a situação a seu favor. Depois de encontrar uma eterna amizade no guerreiro conhecido como Master Yi, Wukong se tornou o último aprendiz da ancestral arte marcial conhecida como Wu Ju. Armado com um cajado encantado, Wukong tenta evitar que Ionia caia em ruína.",
+    "photoCover":"https://streamie.com.br/wp-content/uploads/2017/03/img-wukong-skin.jpg",
+  },
+  {
+    "id":"4",
+    "title": "Yasuo: o Imperdoável",
+    "description": "Yasuo, um ioniano extremamente determinado, também é um ágil espadachim que empunha o próprio ar contra seus inimigos. Quando jovem, ele teve seu orgulho ferido ao ser acusado injustamente do assassinato de seu mestre e, sem conseguir provar sua inocência, foi forçado a matar o próprio irmão para se defender. Com o passar do tempo, a identidade da pessoa responsável pelo assassinato de seu mestre foi revelada e seu irmão retornou inexplicavelmente da morte, mas Yasuo ainda não consegue se perdoar. Agora, ele vaga pelo mundo com nada mais do que o vento para guiar sua espada.",
+    "photoCover":"https://images.contentstack.io/v3/assets/blt187521ff0727be24/bltbfccf51206688048/60ee111cfdd68748b2b7cd44/promo-yasuo-image.jpg",
+  },
+  {
+    "id":"5",
+    "title": "Zeri: a faísca de zaun",
+    "description": "Uma jovem obstinada e vivaz da classe operária de Zaun, Zeri canaliza magia elétrica para carregar a si mesma e a uma arma personalizada. O poder volátil espelha o que ela sente, as centelhas refletindo o estilo de vida ultrarrápido dela. Profundamente compassiva para com os outros, Zeri leva o amor da família e do lar dela consigo a todas as lutas. Embora as tentativas de ajudar saiam pela culatra às vezes, Zeri acredita piamente em uma verdade: lute pela sua comunidade, e ela lutará com você.",
+    "photoCover":"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Zeri_0.jpg",
+  },
+  {
+    "id":"6",
+    "title": "Thresh: o guardião das correntes",
+    "description": "Sádico e astuto, Thresh é um espírito ambicioso e incansável das Ilhas das Sombras. Ele já foi guardião de incontáveis segredos arcanos, mas acabou sucumbindo a poderes maiores do que a vida e a morte. Agora, Thresh vive atormentando e corrompendo vítimas de forma lenta e dolorosamente criativa. Seus alvos sofrem muito além de seus corpos mortais, pois Thresh aflige as próprias almas, aprisionando-as em sua lanterna maldita para torturá-las por toda a eternidade.",
+    "photoCover":"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Thresh_0.jpg",
+  }
+]
